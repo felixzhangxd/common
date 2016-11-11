eeb13571b344ac53d2471ac344405bde2ea739e4
felix <178588314@qq.com>
2016-11-11 12:28:30 +0800

    tmp

--- common-tag/src/main/java/com/firebugsoft/common/tag/bootstrap/PageTag.java
+++ common-tag/src/main/java/com/firebugsoft/common/tag/bootstrap/PageTag.java
@@ -7,9 +7,17 @@ import javax.servlet.jsp.tagext.Tag;
 import javax.servlet.jsp.tagext.TagSupport;
 
 public class PageTag extends TagSupport {
-    private int    pn;
+    private int    pn = 1;
     private int    maxpn;
-    private String form;
+    private String formId;
+
+    public PageTag() {}
+
+    public PageTag(int pn, int maxpn, String formId) {
+        this.pn = pn;
+        this.maxpn = maxpn;
+        this.formId = formId;
+    }
 
     public int getPn() {
         return pn;
@@ -27,32 +35,42 @@ public class PageTag extends TagSupport {
         this.maxpn = maxpn;
     }
 
-    public String getForm() {
-        return form;
+    public String getFormId() {
+        return formId;
     }
 
-    public void setForm(String form) {
-        this.form = form;
+    public void setFormId(String formId) {
+        this.formId = formId;
     }
 
     @Override
     public int doStartTag() throws JspException {
-        String prev = this.pn == 1 ? " class=\"disabled\"" : "";
-        String next = this.pn == this.maxpn ? " class=\"disabled\"" : "";
+        String hiddenId = formId + "-pn";
         StringBuilder html = new StringBuilder();
-        html.append("<input type=\"hidden\" name=\"pn\" value=\"" + this.pn + "\">");
+        html.append("<input type=\"hidden\" name=\"pn\" value=\"" + this.pn + "\" id=\""+hiddenId+"\" form=\""+formId+"\">");
         html.append("<ul class=\"pagination\">");
-        html.append("<li" + prev + "><a href=\"javascript:void(0)\" onclick=\"pageSubmit(1)\"><span class=\"glyphicon glyphicon-step-backward\"></span></a></li>");
-        html.append("<li" + prev + "><a href=\"javascript:void(0)\" onclick=\"pageSubmit(" + (this.pn - 1) + ")><span class=\"glyphicon glyphicon-arrow-left\"></span></a></li>");
+        boolean canPrev = this.pn > 1;
+        if(canPrev) {
+            html.append("<li><a href=\"javascript:void(0)\" onclick=\"pageSubmit(1)\"><span class=\"glyphicon glyphicon-step-backward\"></span></a></li>");
+            html.append("<li><a href=\"javascript:void(0)\" onclick=\"pageSubmit(" + (this.pn - 1) + ")\"><span class=\"glyphicon glyphicon-arrow-left\"></span></a></li>");
+        } else {
+            html.append("<li class=\"disabled\"><span class=\"glyphicon glyphicon-step-backward\"></span></li>");
+            html.append("<li class=\"disabled\"><span class=\"glyphicon glyphicon-arrow-left\"></span></li>");
+        }
         html.append("<li class=\"active\"><a href=\"javascript:void(0)\">" + this.pn + "</a></li>");
-        html.append("<li" + next + "><a href=\"javascript:void(0)\" onclick=\"pageSubmit(" + (this.pn + 1) + ")><span class=\"glyphicon glyphicon-arrow-right\"></span></a></li>");
-        html.append("<li" + next + "><a href=\"javascript:void(0)\" onclick=\"pageSubmit(" + maxpn + ")><span class=\"glyphicon glyphicon-step-forward\"></span></a></li>");
+        boolean canNext = this.pn < this.maxpn;
+        if(canNext) {
+            html.append("<li><a href=\"javascript:void(0)\" onclick=\"pageSubmit(" + (this.pn + 1) + ")\"><span class=\"glyphicon glyphicon-arrow-right\"></span></a></li>");
+            html.append("<li><a href=\"javascript:void(0)\" onclick=\"pageSubmit(" + maxpn + ")\"><span class=\"glyphicon glyphicon-step-forward\"></span></a></li>");
+        } else {
+            html.append("<li class=\"disabled\"><span class=\"glyphicon glyphicon-arrow-right\"></span></li>");
+            html.append("<li class=\"disabled\"><span class=\"glyphicon glyphicon-step-forward\"></span></li>");
+        }
         html.append("</ul>");
         html.append("<script type=\"text/javascript\">");
         html.append("function pageSubmit(pn){");
-        html.append("  alert(\"fuck\");");
-        html.append("  document.getElementById('" + form + "').pn.value=pn;");
-        html.append("  document.getElementById('" + form + "').submit();");
+        html.append("  document.getElementById('" + hiddenId + "').value=pn;");
+        html.append("  document.getElementById('" + formId + "').submit();");
         html.append("}");
         html.append("</script>");
         try {
